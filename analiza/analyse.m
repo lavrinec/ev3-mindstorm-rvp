347df2f55772d56f2507dfde91beaafa9996b6a6
lavrinec <ml9987@student.uni-lj.si>
2019-05-12 23:22:42 +0200

    analiza png added

--- analiza/analyse.m
+++ analiza/analyse.m
@@ -1,27 +1,21 @@
-path = 'C:\Users\Ivan\Documents\FRI\Magisterij\1.Letnik\RVP\vaje\analiza';
-
-T1 = read(path, 'meritve0');
+path = 'rotate180';
+fileName = '502010';
+saveTo = fullfile(path,strcat('\', fileName,'.png'));
+T1 = read(path, fileName);
 %koti so negativni, popravi naslednjic pri izpisu
 T1(:,1) = T1(:,1)*-1;
 
-T2 = read(path, 'meritve01');
+T2 = read(path, '005000');
 %koti so negativni, popravi naslednjic pri izpisu
 T2(:,1) = T2(:,1)*-1;
-
-T3 = readE(path, 'meritveC01');
-T4 = readE(path, 'meritveC0');
 
 figure(1); 
 hold on;
 plot(T1(:,2), T1(:,1), 'r')
-hold on;
-plot(T2(:,2), T2(:,1), 'b')
+saveas(gcf,saveTo)
+% hold on;
+% plot(T2(:,2), T2(:,1), 'b')
 
-hold on;
-plot(T3(:,3), T3(:,1))
-hold on;
-plot(T4(:,3), T4(:,1))
-
-hold off;
-legend('I=0','I=0.1', 'counter, I=0', 'counter, I=0.1')
+% hold off;
+% legend('I=0','I=0.1')
 
